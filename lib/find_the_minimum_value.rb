e1dd91ccd8ee1979a349b6c79cf206659040e9da
Michael Sotir <sotirm@umich.edu>
2020-04-30 02:56:18 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,3 +1,9 @@
 def find_min_value(array)
-  # Add your solution here
+  counter = 0 
+  min_val = 0 
+  while counter < array.length do
+    
+    if min_val > array[counter]
+    counter = counter + 1  
+  end
 end
